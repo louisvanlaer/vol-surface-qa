f29345f834ef507d000c116304935dfa6a43301c
louisvanlaer <louis.vanlaer1@gmail.com>
2026-08-15 11:59:34 +0200

    fourth commit

---
+++ fonction.py
@@ -0,0 +1,21 @@
+import json
+import calendar
+from datetime import date
+
+def load_json_file(file_path):     
+    with open(file_path, "r", encoding="utf-8") as file:
+        data = json.load(file)
+    return data
+
+def choose_date():
+    annee, mois = 2026, 8
+
+    dernier_jour =  calendar.monthrange(annee, mois)[1]
+
+    debut_mois = date(annee, mois, 1)
+    fin_mois = date(annee, mois, dernier_jour)
+
+    return debut_mois, fin_mois
+
+
+
--- user-interface.py
+++ user-interface.py
@@ -1,9 +1,20 @@
 import streamlit as st
+import pandas as pd
+from datetime import date
+
+
+
+from fonction import load_json_file
+from fonction import choose_date
+
+
 
 def _reset_run():
     st.session_state.run = False
 
 def main():
+
+    debut_aout, fin_aout = choose_date()
 
     st.title("Volatility Surface")
 
@@ -23,21 +34,44 @@ def main():
             if uploaded_file is not None:
                 request_path = uploaded_file.name
                 st.session_state.request_path = request_path
+                data = load_json_file(request_path)
+                underlying = data["underlying"]
+                spot = data["spot"]
+                surface = data["surface"]
+                
+                surface_df = pd.DataFrame(data["surface"])
 
         except AttributeError:
             st.warning("Please upload a valid JSON file.")
 
-        choose_instrument = st.text_input("Enter an instrument name", value="AAPL", on_change=_reset_run)
+        choose_instrument = st.text_input("Enter an instrument name", value="DEMO_ASSET", on_change=_reset_run)
         threshold = 0
 
         tolerance = st.text_input("Tolerance(%)", value="0.01", on_change=_reset_run)
 
         col1, col2 = st.columns(2)
         with col1:
-            Date1 = st.date_input("Start Date", on_change=_reset_run)
+            Date1 = st.date_input(
+                "Start Date",
+                value=debut_aout,
+                min_value=debut_aout,
+                max_value=fin_aout,
+                on_change=_reset_run,
+            )
         with col2: 
-            Date2 = st.date_input("End Date", on_change=_reset_run)
+            Date2 = st.date_input(
+                "End Date",
+                value=fin_aout,
+                min_value=debut_aout,
+                max_value=fin_aout,
+                on_change=_reset_run,
+            )
 
+    st.dataframe(surface_df, use_container_width=True)
+    
+    # st.write("Instrument :", underlying)
+    # st.write("Spot :", spot)
+    # st.write("Surface :", surface)
 
 
 
